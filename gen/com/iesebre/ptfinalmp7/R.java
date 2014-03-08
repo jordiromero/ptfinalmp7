4cb7041a6b7872ca146883c9a8081cb184887f3f
jordiromero <jordiromero@iesebre.com>
2014-03-08 20:42:51 +0100

    afegim activity condicions

--- gen/com/iesebre/ptfinalmp7/R.java
+++ gen/com/iesebre/ptfinalmp7/R.java
@@ -1132,20 +1132,23 @@ containing a value of this type.
         public static final int abc_popup_menu_item_layout=0x7f030014;
         public static final int abc_search_dropdown_item_icons_2line=0x7f030015;
         public static final int abc_search_view=0x7f030016;
-        public static final int activity_dash_board_principal=0x7f030017;
-        public static final int activity_main=0x7f030018;
-        public static final int control_login=0x7f030019;
-        public static final int formulari_layout=0x7f03001a;
-        public static final int fragment_main=0x7f03001b;
-        public static final int mapa_layout=0x7f03001c;
-        public static final int menu_layout=0x7f03001d;
-        public static final int principal_layout=0x7f03001e;
-        public static final int reserva_layout=0x7f03001f;
-        public static final int support_simple_spinner_dropdown_item=0x7f030020;
+        public static final int activity_condicions=0x7f030017;
+        public static final int activity_dash_board_principal=0x7f030018;
+        public static final int activity_main=0x7f030019;
+        public static final int control_login=0x7f03001a;
+        public static final int formulari_layout=0x7f03001b;
+        public static final int fragment_condicions=0x7f03001c;
+        public static final int fragment_main=0x7f03001d;
+        public static final int mapa_layout=0x7f03001e;
+        public static final int menu_layout=0x7f03001f;
+        public static final int principal_layout=0x7f030020;
+        public static final int reserva_layout=0x7f030021;
+        public static final int support_simple_spinner_dropdown_item=0x7f030022;
     }
     public static final class menu {
-        public static final int dash_board_principal=0x7f0c0000;
-        public static final int main=0x7f0c0001;
+        public static final int condicions=0x7f0c0000;
+        public static final int dash_board_principal=0x7f0c0001;
+        public static final int main=0x7f0c0002;
     }
     public static final class string {
         public static final int Cancelar=0x7f0a0011;
@@ -1191,6 +1194,7 @@ containing a value of this type.
          */
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int acepta_condi=0x7f0a001b;
+        public static final int acepta_condicions=0x7f0a0024;
         public static final int action_settings=0x7f0a000e;
         public static final int app_name=0x7f0a000d;
         public static final int carta=0x7f0a0020;
@@ -1207,6 +1211,7 @@ containing a value of this type.
         public static final int registrat=0x7f0a0012;
         public static final int repassword=0x7f0a0018;
         public static final int reserva=0x7f0a001f;
+        public static final int title_activity_condicions=0x7f0a0023;
         public static final int title_activity_dash_board_principal=0x7f0a0022;
         public static final int ubicacio=0x7f0a0021;
         public static final int usuari=0x7f0a001c;
--- src/com/iesebre/ptfinalmp7/FormulariActivity.java
+++ src/com/iesebre/ptfinalmp7/FormulariActivity.java
@@ -1,12 +1,29 @@
 package com.iesebre.ptfinalmp7;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class FormulariActivity extends Activity {
+	private TextView condicions;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.formulari_layout);
+		
+		TextView condicions = (TextView)findViewById(R.id.textViewCondicions);
+		condicions.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent condicions = new Intent(FormulariActivity.this, CondicionsActivity.class );
+				startActivity(condicions);
+				
+			}
+		});
+		
 	}
 }
