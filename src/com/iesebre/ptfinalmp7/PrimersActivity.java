4c8046babb723be35bda3d0b9b3d4ee865a94d98
jordiromero <jordiromero@iesebre.com>
2014-03-09 18:17:41 +0100

    posem en marxa el tabhost

---
+++ src/com/iesebre/ptfinalmp7/PrimersActivity.java
@@ -0,0 +1,14 @@
+package com.iesebre.ptfinalmp7;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class PrimersActivity extends ListActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_primers);
+	}
+
+}
